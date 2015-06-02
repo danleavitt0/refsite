11ec2d7bf542fe19e1148db9ab7b3eb575aa7f60
Daniel Leavitt <danleavitt0@gmail.com>
2015-06-02 21:04:53 +0000

    card style

--- lib/Card.jsx
+++ lib/Card.jsx
@@ -16,21 +16,29 @@ var Card = React.createClass(Radium.wrap({
 			? <CardTitle style={{backgroundColor:this.props.color}} content={this.props.title} /> 
 			: null
 		return (
-			<Paper style={[
+			<div style={[
 				styles.card,
 				this.props.style
 			]} >
 				{title}
 				<CardContent content={this.props.children} />
-			</Paper>
+			</div>
 		)
 	}
 }))
 
 var styles = {
 	card: {
+		fontFamily:'Roboto, sans-serif',
 		margin:'15px 0',
-		backgroundColor:'#fff'
+		backgroundColor:'#fff',
+		boxShadow: '0 1px 4px 0 rgba(0,0,0,0.37)',
+		borderRadius:2,
+		transition:'box-shadow .3s cubic-bezier(0.4,0,0.2,1)',
+		cursor:'pointer',
+		':hover': {
+			boxShadow: '0 0 3px rgba(0,0,0,.13),0 3px 6px rgba(0,0,0,.26)'
+		}
 	}
 }
 
